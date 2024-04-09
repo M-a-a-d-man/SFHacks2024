bc75fc9f3558eb51a3ab0134155fd128215faa8c
Codingjackking <nainghtet123@gmail.com>
2024-04-08 19:12:40 -0700

    Removed the api key

--- report_incident_app/app/db.js
+++ report_incident_app/app/db.js
@@ -2,13 +2,7 @@ import mongoose from 'mongoose';
 
 async function connectDB(){
     try{
-        const password = 'j9yumg8HxZ1lSThP'; // Your actual password
-        const encodedPassword = encodeURIComponent(password);
-
-        // Use the encoded passexitword in the connection string
-        // const url = `mongodb+srv://nainghtet0123:${encodedPassword}@sfsu2024.5vhnv57.mongodb.net/?retryWrites=true&w=majority&appName=SFSU2024`;
-        const url = `mongodb+srv://nainghtet0123:${encodedPassword}@sfsu2024.5vhnv57.mongodb.net/danger_zone?retryWrites=true&w=majority&appName=SFSU2024`;
-
+        
         await mongoose.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
